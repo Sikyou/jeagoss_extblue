51e3008e057b96abd0ef5cfce37339543545f2b1
Jef Oliver <jgoliver@jeago.com>
2010-07-14 23:14:14 -0600

    Fixups

--- products/jeagoss_extblue_passion.mk
+++ products/jeagoss_extblue_passion.mk
@@ -4,7 +4,7 @@ $(call inherit-product, vendor/cyanogen/products/cyanogen_passion.mk)
 # Extremely Blue build time theming vendor setup
 # Add Extremely Blue Overrides
 PRODUCT_NAME := jeagoss_extblue_passion
-PRODUCT_PACKAGE_OVERLAYS += vendor/jeagoss_extblue/overlay
+PRODUCT_PACKAGE_OVERLAYS := vendor/jeagoss_extblue/overlay
 
 PRODUCT_COPY_FILES +=  \
     vendor/jeagoss_extblue/proprietary/ThemeDefaults.apk:system/app/ThemeDefaults.apk
