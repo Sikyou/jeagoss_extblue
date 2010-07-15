8aeafd3779f45bbd4681c1a397c15de1522f14b4
Jef Oliver <jgoliver@jeago.com>
2010-07-14 23:40:03 -0600

    typos

--- products/jeagoss_extblue_passion.mk
+++ products/jeagoss_extblue_passion.mk
@@ -4,7 +4,8 @@ $(call inherit-product, vendor/cyanogen/products/cyanogen_passion.mk)
 # Extremely Blue build time theming vendor setup
 # Add Extremely Blue Overrides
 PRODUCT_NAME := jeagoss_extblue_passion
-PRODUCT_PACKAGE_OVERLAYS := vendor/jeagoss_extblue/overlay
+PRODUCT_PACKAGE_OVERLAYS := vendor/jeagoss_extblue/overlay/common
+PRODUCT_PACKAGE_OVERLAYS += vendor/jeagoss_extblue/overlay/passion
 
 PRODUCT_COPY_FILES +=  \
     vendor/jeagoss_extblue/proprietary/ThemeDefaults.apk:system/app/ThemeDefaults.apk
--- products/jeagoss_extblue_sholes.mk
+++ products/jeagoss_extblue_sholes.mk
@@ -4,7 +4,7 @@ $(call inherit-product, vendor/cyanogen/products/cyanogen_sholes.mk)
 # Extremely Blue build time theming vendor setup
 # Add Extremely Blue Overrides
 PRODUCT_NAME := jeagoss_extblue_sholes
-PRODUCT_PACKAGE_OVERLAYS += vendor/jeagoss_extblue/overlay
+PRODUCT_PACKAGE_OVERLAYS := vendor/jeagoss_extblue/overlay/common
 
 PRODUCT_COPY_FILES +=  \
     vendor/jeagoss_extblue/proprietary/ThemeDefaults.apk:system/app/ThemeDefaults.apk
