85d8776b3ebbbb2c83e9699577387a7938b0b998
Jef Oliver <jgoliver@jeago.com>
2010-07-18 02:57:10 -0600

    D/S Additions

--- products/jeagoss_extblue_dream_sapphire.mk
+++ products/jeagoss_extblue_dream_sapphire.mk
@@ -5,6 +5,7 @@ $(call inherit-product, vendor/cyanogen/products/cyanogen_dream_sapphire.mk)
 # Add Extremely Blue Overrides
 PRODUCT_NAME := jeagoss_extblue_dream_sapphire
 PRODUCT_PACKAGE_OVERLAYS := vendor/jeagoss_extblue/overlay/common
+PRODUCT_PACKAGE_OVERLAYS += vendor/jeagoss_extblue/overlay/dream_sapphire
 
 PRODUCT_COPY_FILES +=  \
     vendor/jeagoss_extblue/proprietary/ThemeDefaults.apk:system/app/ThemeDefaults.apk \
